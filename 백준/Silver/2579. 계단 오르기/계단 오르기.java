36bc9ede90676fed31141dce98bf2b2fcbf50df0
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2024-02-05 18:43:19 +0900

    [Silver III] Title: 계단 오르기, Time: 236 ms, Memory: 18108 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int stair[] = new int[301];
+        int score[] = new int[301];
+        for(int i = 1; i <= num; i++){
+            stair[i] = sc.nextInt();
+        }
+        score[1] = stair[1];
+        score[2] = stair[1] + stair[2];
+        score[3] = Math.max(stair[2],stair[1])
+                + stair[3];
+
+        for(int i = 4; i<=num; i++){
+            score[i] = Math.max(score[i-3] + stair[i-1], score[i-2])
+                    + stair[i];
+        }
+        System.out.println(score[num]);
+    }
+}
