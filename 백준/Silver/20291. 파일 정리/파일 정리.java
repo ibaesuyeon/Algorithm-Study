29c156bdb8687697371554b978ce82c7ec5e61bb
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2024-07-02 11:32:16 +0900

    [Silver III] Title: 파일 정리, Time: 1264 ms, Memory: 60300 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            st.nextToken(".");
+            String extend = st.nextToken(".");
+            if (map.containsKey(extend))
+                map.put(extend, map.get(extend)+1);
+            else map.put(extend, 1);
+        }
+        List<String> keySet = new ArrayList<>(map.keySet());
+        Collections.sort(keySet);
+        for(String key : keySet) {
+            System.out.print(key +" ");
+            System.out.println(map.get(key));
+        }
+    }
+}
