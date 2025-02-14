6048316b7a4a3788ed0baa01d6efa3df8e014eea
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2025-02-14 14:36:19 +0900

    [Silver IV] Title: 스택 2, Time: 1248 ms, Memory: 310964 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,35 +1,40 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<n; i++) {
-            int command = sc.nextInt();
-            switch(command) {
-                case (1) :
-                    stack.push(sc.nextInt()); break;
-                case (2) :
+        for (int i = 0; i<n; i++){
+            String[] command = br.readLine().split(" ");
+            switch (command[0]){
+                case "1":
+                    stack.addLast(Integer.parseInt(command[1]));
+                    break;
+                case "2":
                     if (stack.isEmpty())
-                        sb.append(-1).append("\n");
-                    else sb.append(stack.pop()).append("\n");
+                        sb.append(-1+"\n");
+                    else sb.append((stack.removeLast())+"\n");
                     break;
-                case (3) :
-                    sb.append(stack.size()).append("\n");
+                case "3":
+                    sb.append((stack.size())+"\n");
                     break;
-                case (4) :
+                case "4":
                     if (stack.isEmpty())
-                        sb.append(1).append("\n");
-                    else sb.append(0).append("\n");
+                        sb.append((1)+"\n");
+                    else sb.append((0)+"\n");
                     break;
-                case (5) :
-                    if (stack.isEmpty()) sb.append(-1).append("\n");
-                    else sb.append(stack.peek()).append("\n");
+                case "5":
+                    if (stack.isEmpty())
+                        sb.append((-1)+"\n");
+                    else sb.append((stack.getLast())+"\n");
                     break;
+                default: break;
             }
         }
         System.out.println(sb);
