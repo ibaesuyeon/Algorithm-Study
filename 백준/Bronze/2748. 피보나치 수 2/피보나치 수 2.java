d03aa30a12169ba0e5522560e5b5d2a8a333160b
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2025-05-21 14:00:49 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -3,22 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static long[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        dp = new long[91];
-        for(int i = 0; i<91; i++){
-            dp[i] = -1;
+        long[] dp = new long[n+1];
+        dp[0] = 0; dp[1] = 1;
+        for(int i = 2; i<n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        dp[0] = 0; dp[1] = 1; dp[2] = 1;
-        dp[n] = fibo(n);
         System.out.println(dp[n]);
-
-    }
-    public static long fibo(int n){
-        if(dp[n] == -1)
-            dp[n] = fibo(n-1) + fibo(n-2);
-        return dp[n];
     }
 }
