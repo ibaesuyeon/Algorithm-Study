245aac3d43e7945cc5adecceabb7398cc5443ef0
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2025-09-23 17:12:05 +0900

    [level 0] Title: 옹알이 (1), Time: 7.49 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,17 +2,11 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for(int i = 0; i<babbling.length; i++){
-            while(true){
-                if(babbling[i].contains("aya")){
-                    babbling[i] = babbling[i].replaceFirst("aya", "-");
-                } else if(babbling[i].contains("ye")){
-                    babbling[i] = babbling[i].replaceFirst("ye", "-");
-                } else if(babbling[i].contains("woo")){
-                    babbling[i] = babbling[i].replaceFirst("woo", "-");
-                } else if(babbling[i].contains("ma")){
-                    babbling[i] = babbling[i].replaceFirst("ma", "-");
-                } else break; 
-            }
+            babbling[i] = babbling[i].replaceFirst("aya", "-");
+            babbling[i] = babbling[i].replaceFirst("ye", "-");
+            babbling[i] = babbling[i].replaceFirst("woo", "-");
+            babbling[i] = babbling[i].replaceFirst("ma", "-");
+            
             babbling[i] = babbling[i].replaceAll("-", "");
             if(babbling[i].equals("")){
                 answer ++;
