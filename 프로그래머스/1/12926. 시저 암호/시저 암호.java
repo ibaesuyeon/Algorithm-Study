04ce58a1bf4a1c34fac9041b2c736a659ff59ae1
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2025-11-13 06:19:13 +0900

    [level 1] Title: 시저 암호, Time: 26.59 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,30 +2,23 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         char[] arr = s.toCharArray();
+        
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] != ' '){
-                int ascii = 0;
-                if(Character.isUpperCase(arr[i])){
-                    ascii = (int)(arr[i] - 'A');
-                    ascii += n;
-                    if(ascii >= 26){
-                        ascii -= 26; //알파벳 개수만큼 빼기
-                    }
-                    arr[i] = (char)(ascii+'A');
-
-                } else {
-                    ascii = (int)(arr[i] - 'a');
-                    ascii += n;
-                    if(ascii >= 26){ 
-                        ascii -= 26; //알파벳 개수만큼 빼기
-                    }
-                    arr[i] = (char)(ascii+'a');
-                }
+            if(arr[i] == ' ')
+                continue;
+        
+            if(Character.isUpperCase(arr[i])){
+                arr[i] = (char)((arr[i] - 'A' + n) % 26 + 'A');    
+                
+            } else if (Character.isLowerCase(arr[i])){
+                arr[i] = (char)((arr[i] - 'a' + n) % 26 + 'a');
             }
         }
+
         for (char c : arr) {
             answer += c;
         }
+        
         return answer;
     }
 }
