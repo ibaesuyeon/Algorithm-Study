413d9e42b921999d89f43a03ab444396ae97bb1a
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2025-11-15 15:08:06 +0900

    [level 1] Title: 햄버거 만들기, Time: 61.59 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -5,19 +5,20 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         for(int i = 0; i<ingredient.length; i++){
             stack.add(ingredient[i]);
+            
             int size = stack.size();
             if(size >= 4){
-                if(stack.get(size-1) == 1 && stack.get(size-4) == 1){
-                    if(stack.get(size-2) == 3 && stack.get(size-3) == 2){
-                        for(int j = 0; j<4; j++){
-                            stack.pop();
-                        }
-                        answer ++;
+                if(stack.get(size-1) == 1 
+                   && stack.get(size-2) == 3
+                   && stack.get(size-3) == 2
+                   && stack.get(size-4) == 1){
+                    for(int j = 0; j<4; j++){
+                        stack.pop();
                     }
+                    answer ++;
                 }
             }
         }
-        
         return answer;
     }
 }
